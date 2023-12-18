f42c6c7db1f1d99905141e75ec44474a95bf7817
rohit <mailsrohitk@gmail.com>
2023-12-19 01:18:00 +0530

    contact page created

--- client/src/Components/Layouts/Footer.js
+++ client/src/Components/Layouts/Footer.js
@@ -7,11 +7,10 @@ const Footer = () => {
       <h4 className="text-center">
         All rights reserved &copy; ROHIT KUMAR SINGH
       </h4>
-      <br/>
       <p className="text-center at-3">
         <Link to="/about">About</Link>   |  
-        <Link to="/about">Contact</Link>  | 
-        <Link to="/about">Privacy Policy</Link>
+        <Link to="/contact">Contact</Link>  | 
+        <Link to="/policy">Privacy Policy</Link>
       </p>
     </div>
   );
--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -1,12 +1,24 @@
-import React from 'react'
+// Contact.js
+import React from 'react';
 import Layout from "../Components/Layouts/Layout.js";
+import contactImage from "../images/contact.png"; 
 
 const Contact = () => {
   return (
     <Layout>
-      <h1>Contact Page</h1>
+      <div className="container text-center">
+        <div className="contact-container">
+          <img src={contactImage} alt="Contact" className="contact-image" />
+          <div className="contact-details">
+            <h2>Contact Details:</h2>
+            <p>Email: example@example.com</p>
+            <p>Phone: +1 (123) 456-7890</p>
+            <p>Address: 123 Main Street, City, Country</p>
+          </div>
+        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
